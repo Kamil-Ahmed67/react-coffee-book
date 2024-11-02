49934a05b83f24d1f2a1229894c8b94aa4662554
Kamil-Ahmed67 <nabilahmedtalukder6272@gmail.com>
2024-11-03 00:45:15 +0600

    Project Basic Components Installed

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <>
+      <h2 className='text-5xl'>React</h2>
+      <button>Sumbit</button>
+    </>
+  )
+}
+
+export default App
---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,16 @@
+import Footer from "../components/Footer/Footer";
+import Navbar from "../components/Navbar/Navbar";
+
+const MainLayout = () => {
+    return (
+        <div>
+            {/* Navbar */}
+            <Navbar></Navbar>
+            {/* Dynamic Section */}
+            {/* Footer */}
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default MainLayout;
